d75a41efa562ee68f94d7ea6d04b699b5a0cf2d6
김요한 <rladygks1210@naver.com>
2025-10-11 23:55:12 +0900

    [feat] ID를 이용해서 특정 게시물 가져오기

--- section02/nestjs-board-app/src/boards/boards.controller.ts
+++ section02/nestjs-board-app/src/boards/boards.controller.ts
@@ -13,10 +13,11 @@ import { BoardsService } from './boards.service';
 import { BoardStatus } from './board-status.enum';
 import { CreateBoardDto } from './dto/create-board.dto';
 import { BoardStatusValidationPipe } from './pipes/board-status-validation.pipe';
+import { Board } from './board.entity';
 
 @Controller('boards')
 export class BoardsController {
-  // constructor(private boardService: BoardsService) {}
+  constructor(private boardService: BoardsService) {}
   // @Get('/')
   // getAllBoard(): Board[] {
   //   return this.boardService.getAllBoards();
@@ -26,6 +27,12 @@ export class BoardsController {
   // createBoard(@Body() createBoardDto: CreateBoardDto): Board {
   //   return this.boardService.createBoard(createBoardDto);
   // }
+
+  @Get('/:id')
+  getBoardById(@Param('id') id: number): Promise<Board> {
+    return this.boardService.getBoardById(id);
+  }
+
   // @Get('/:id')
   // getBoardById(@Param('id') id: string): Board {
   //   return this.boardService.getBoardById(id);
